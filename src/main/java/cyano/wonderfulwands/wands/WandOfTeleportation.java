a7bc13afe4b6f480e6ceffc459f21b8bd11720ee
CCHall <explosivegnome@yahoo.com>
2015-02-22 19:41:59 -0500

    Made the wand of teleportation 20% cooler

--- src/main/java/cyano/wonderfulwands/wands/WandOfTeleportation.java
+++ src/main/java/cyano/wonderfulwands/wands/WandOfTeleportation.java
@@ -8,7 +8,10 @@ import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.FMLLog;
 
 public class WandOfTeleportation extends Wand {
 	public static final String itemName = "wand_teleportation";
@@ -58,10 +61,44 @@ public class WandOfTeleportation extends Wand {
         	}
         	srcItemStack.damageItem(getUseCost(), playerEntity);
         }
-		if (!world.isRemote)
-        {
-			world.spawnEntityInWorld(new EntityEnderPearl(world, playerEntity));
-        }
+
+	        playSound("mob.endermen.portal",world,playerEntity);
+			
+			final int maxRange = 160;
+			double x = playerEntity.getPositionVector().xCoord;
+			double y = playerEntity.getPositionVector().yCoord + 1.4;
+			double z = playerEntity.getPositionVector().zCoord;
+			double dx = (double)(-MathHelper.sin(playerEntity.rotationYaw / 180.0F * (float)Math.PI) * MathHelper.cos(playerEntity.rotationPitch / 180.0F * (float)Math.PI));
+        	double dy = (double)(-MathHelper.sin(playerEntity.rotationPitch / 180.0F * (float)Math.PI));
+        	double dz = (double)( MathHelper.cos(playerEntity.rotationYaw / 180.0F * (float)Math.PI) * MathHelper.cos(playerEntity.rotationPitch / 180.0F * (float)Math.PI));
+
+        	
+			for(int i = 0; i < maxRange; i++){
+				double nx = x + dx;
+				double ny = y + dy;
+				double nz = z + dz;
+				if(ny < 0 || ny > 255) break;
+				BlockPos coord = new BlockPos(nx,ny,nz);
+				if(world.isAreaLoaded(coord, 1, true) && world.isAirBlock(coord)){
+					world.spawnParticle(EnumParticleTypes.PORTAL, 
+							x + (world.rand.nextDouble() - 0.5), y + (world.rand.nextDouble() - 0.5), z + (world.rand.nextDouble() - 0.5), 
+							(world.rand.nextFloat() - 0.5f) * 0.2f, (world.rand.nextFloat() - 0.5f) * 0.2f, (world.rand.nextFloat() - 0.5f) * 0.2f, 
+							new int[0]);
+					x = nx;
+					y = ny;
+					z = nz;
+					continue;
+				} else{
+					x = coord.getX() + 0.5;
+					y = coord.getY() + 1.0625;
+					z = coord.getZ() + 0.5;
+					break;
+				}
+			}
+
+			playerEntity.setLocationAndAngles(x, y, z,playerEntity.rotationYaw, playerEntity.rotationPitch);
+
+	        playSound("mob.endermen.portal",world,playerEntity);
 	}
 
 }
