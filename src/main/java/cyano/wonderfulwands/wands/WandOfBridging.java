ec2155a828ebb9fe7a352babb158a0f620d7091e
Chris Hall <explosivegnome@yahoo.com>
2016-04-17 14:07:25 -0400

    fixed wands of healing and bridging

--- src/main/java/cyano/wonderfulwands/wands/WandOfBridging.java
+++ src/main/java/cyano/wonderfulwands/wands/WandOfBridging.java
@@ -8,7 +8,10 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -20,7 +23,7 @@ public class WandOfBridging extends Wand {
 	
 	public static int defaultCharges = 64;
 	
-	final int limit = 32;
+	private final int limit = 32;
 	
 	private final Block bridgeBlock;
 	
@@ -42,39 +45,54 @@ public class WandOfBridging extends Wand {
 	public int getBaseRepairCost() {
 		return 2;
 	}
-	@Override public boolean onItemUse(ItemStack srcItemStack, EntityPlayer playerEntity, World world, BlockPos coord, EnumFacing blockFace, float px, float py, float pz){
-		return true;
+
+	@Override public int getMaxItemUseDuration(ItemStack par1ItemStack){
+		return 7200;
+	}/**
+	 * Callback for item usage, invoked when right-clicking on a block. If the item
+	 * does something special on right clicking, he will have one of those. Return
+	 * True if something happen and false if it don't. This is for ITEMS, not BLOCKS
+	 */
+	@Override public boolean onItemUse(ItemStack srcItemStack, EntityPlayer playerEntity, World world, BlockPos coord, EnumFacing blockFace, float par8, float par9, float par10){
+		return false;
 	}
-	@Override public ItemStack onItemUseFinish (ItemStack srcItemStack, World world, EntityLivingBase playerEntity){
+
+
+	@Override public ActionResult<ItemStack> onItemRightClick(ItemStack srcItemStack, World world, EntityPlayer playerEntity, EnumHand hand){
+		playerEntity.setActiveHand(hand);
+		return  new ActionResult(EnumActionResult.SUCCESS, srcItemStack);
+	}
+
+	@Override public void onPlayerStoppedUsing(ItemStack srcItemStack, World world, EntityLivingBase playerEntity, int timeRemain){
 
 		if (playerEntity instanceof EntityPlayer && !((EntityPlayer)playerEntity).capabilities.isCreativeMode)
 		{
 			if(isOutOfCharge(srcItemStack)){
 				// wand out of magic
 				playSound(noChargeAttackSound,world,playerEntity);
-				return srcItemStack;
+				return ;
 			}
 		}
 		int blocksChanged = 0;
-		
+
 		Vec3d delta = playerEntity.getLookVec();
 		delta = (new Vec3d(delta.xCoord,0,delta.zCoord)).normalize();
 		BlockPos coord = playerEntity.getPosition().down();
 		Vec3d originPrime = new Vec3d(coord.getX(), coord.getY(), coord.getZ());
-		
+
 		int[] changeTracker = new int[3];
-		Vec3d pos = originPrime;
+		Vec3d pos = originPrime.add(delta);
 		for(int i = 0; i < limit; i++){
 			int blockDelta = 0;
 			BlockPos block = new BlockPos(pos);
-			
-			
+
+
 			blockDelta += placeBridgeBlock(world,block);
 			blockDelta += placeBridgeBlock(world,block.north());
 			blockDelta += placeBridgeBlock(world,block.south());
 			blockDelta += placeBridgeBlock(world,block.east());
 			blockDelta += placeBridgeBlock(world,block.west());
-			
+
 			changeTracker[0] = changeTracker[1];
 			changeTracker[1] = changeTracker[2];
 			changeTracker[2] = blockDelta;
@@ -83,11 +101,11 @@ public class WandOfBridging extends Wand {
 				break;
 			}
 			blocksChanged += blockDelta;
-			
+
 			// move forward
 			pos = pos.add(delta);
 		}
-		
+
 		if(blocksChanged > 0){
 			playSound(SoundEvents.entity_player_levelup,world,playerEntity);
 			if (playerEntity instanceof EntityPlayer && !((EntityPlayer)playerEntity).capabilities.isCreativeMode)
@@ -96,7 +114,7 @@ public class WandOfBridging extends Wand {
 			}
 		}
 
-		return srcItemStack;
+		return ;
 	}
 	
 	private int placeBridgeBlock(World world, BlockPos coord) {
--- src/main/java/cyano/wonderfulwands/wands/WandOfHealing.java
+++ src/main/java/cyano/wonderfulwands/wands/WandOfHealing.java
@@ -1,7 +1,6 @@
 package cyano.wonderfulwands.wands;
 
 import cyano.wonderfulwands.WonderfulWands;
-import cyano.wonderfulwands.projectiles.DeathSkull;
 import cyano.wonderfulwands.util.RayTrace;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -16,7 +15,6 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
@@ -60,7 +58,7 @@ public class WandOfHealing extends Wand {
 	 * True if something happen and false if it don't. This is for ITEMS, not BLOCKS
 	 */
 	@Override public boolean onItemUse(ItemStack srcItemStack, EntityPlayer playerEntity, World world, BlockPos coord, EnumFacing blockFace, float par8, float par9, float par10){
-		return true;
+		return false;
 	}
 
 	@Override public int getMaxItemUseDuration(ItemStack par1ItemStack){
@@ -81,9 +79,13 @@ public class WandOfHealing extends Wand {
 			}
 		}
 
+		Entity target;
 		RayTraceResult trace = RayTrace.rayTraceBlocksAndEntities(world,range,playerEntity);
-		if(trace == null) return ;
-		Entity target = trace.entityHit;
+		if(trace == null) {
+			target  = playerEntity;
+		}else{
+			target = trace.entityHit;
+		}
 		if(!(target instanceof EntityLivingBase)){
 			target = playerEntity;
 		}
@@ -95,25 +97,13 @@ public class WandOfHealing extends Wand {
 				e.setHealth(Math.min(maxHealth, health + healingAmount) );
 				playSound(SoundEvents.entity_experience_orb_pickup,world,playerEntity);
 				playSound(SoundEvents.entity_player_levelup,world,e);
-				e.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("regeneration"),32,1));
+				e.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("regeneration"),60,0));
+				e.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("glowing"),60,0));
 				if (playerEntity instanceof EntityPlayer && !((EntityPlayer)playerEntity).capabilities.isCreativeMode)
 				{
 					srcItemStack.damageItem(1, playerEntity);
 				}
 			}
-		}
-
-
-		if (!world.isRemote)
-		{
-			double vecX = (double)(-MathHelper.sin(playerEntity.rotationYaw / 180.0F * (float)Math.PI) * MathHelper.cos(playerEntity.rotationPitch / 180.0F * (float)Math.PI));
-			double vecY = (double)(-MathHelper.sin(playerEntity.rotationPitch / 180.0F * (float)Math.PI));
-			double vecZ = (double)( MathHelper.cos(playerEntity.rotationYaw / 180.0F * (float)Math.PI) * MathHelper.cos(playerEntity.rotationPitch / 180.0F * (float)Math.PI));
-
-			double deltaX = (double)(-MathHelper.sin(playerEntity.rotationYaw / 180.0F * (float)Math.PI));
-			double deltaZ = (double)( MathHelper.cos(playerEntity.rotationYaw / 180.0F * (float)Math.PI));
-
-			world.spawnEntityInWorld(new DeathSkull(world, playerEntity,playerEntity.posX+deltaX,playerEntity.posY+1,playerEntity.posZ+deltaZ,  vecX, vecY, vecZ));
 		}
 	}
 
